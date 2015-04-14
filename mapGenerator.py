218ff00c545b57efd6399c7e026715e5357b1eac
shadowfelldown <shadowfelldown@gmail.com>
2015-04-14 06:00:53 -0400

    initial Coding on random map generator. Added random.py to the repository for future use in other methods. map generator populates array of specified size with 1-4 and then displays it as 10 X 10 block.

---
+++ mapGenerator.py
@@ -0,0 +1,12 @@
+__author__ = 'Daniel'
+import random
+def wrap_list(lst, items_per_line=10):
+    lines = []
+    for i in range(0, len(lst), items_per_line):
+        chunk = lst[i:i + items_per_line]
+        line = ", ".join("{!r}".format(x) for x in chunk)
+        lines.append(line)
+    return "[" + ",\n ".join(lines) + "]"
+mapSize = 100
+mapList = [random.randrange(0,5,1) for _ in range (mapSize)]
+print (wrap_list(mapList))
