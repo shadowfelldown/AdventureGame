7d7f3bd5d2e3b188b4b1b5404edd377a0b5ef596
shadowfelldown <shadowfelldown@gmail.com>
2015-04-15 00:07:31 -0400

    changed map generator to be a script full of modules that will be called with map.generate and map.foo

--- WrapList.py
+++ WrapList.py
@@ -1,12 +1,10 @@
 __author__ = 'Daniel'
-import random
 def wrap_list(lst, items_per_line=10):
     lines = []
-    for i in range(0, len(lst), items_per_line):
-        chunk = lst[i:i + items_per_line]
+    lst2 = lst
+    for i in range(0, len(lst2), items_per_line):
+        chunk = lst2[i:i + items_per_line]
         line = ", ".join("{!r}".format(x) for x in chunk)
         lines.append(line)
     return "[" + ",\n ".join(lines) + "]"
-mapSize = 100
-mapList = [random.randrange(0,5,1) for _ in range (mapSize)]
 print (wrap_list(mapList))
