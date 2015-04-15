47df490d514e265f7d9d9c388e374e86a55ca985
shadowfelldown <shadowfelldown@gmail.com>
2015-04-15 17:24:48 -0400

    added a player.py object to house the player functions

--- map.py
+++ map.py
@@ -1,14 +1,12 @@
 __author__ = "Daniel"
-def generate()
+def generate():
     import random
     mapSize = 100
-    mapList = [random.randrange(0,5,1) for _ in range (mapSize)]
+    return [random.randrange(0,5,1) for _ in range (mapSize)]
 def display(lst, items_per_line=10):
     lines = []
-    lst2 = lst
-    for i in range(0, len(lst2), items_per_line):
-        chunk = lst2[i:i + items_per_line]
+    for i in range(0, len(lst), items_per_line):
+        chunk = lst[i:i + items_per_line]
         line = ", ".join("{!r}".format(x) for x in chunk)
         lines.append(line)
     return "[" + ",\n ".join(lines) + "]"
-print (wrap_list(mapList))
