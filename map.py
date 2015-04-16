41360c6cfd9877e6b71128dc0f9352ec7e87a9e6
dlpogue <shadowfelldown@gmail.com>
2015-04-16 15:44:56 -0400

    changed the code insert to just reassignment so that the map length remains 100 after the user is input into the list.

--- map.py
+++ map.py
@@ -1,12 +1,33 @@
 __author__ = "Daniel"
+import random
+
+
 def generate():
-    import random
     mapSize = 100
-    return [random.randrange(0,5,1) for _ in range (mapSize)]
+    return [random.randrange(0, 5, 1) for _ in range(mapSize)]
+
+
 def display(lst, items_per_line=10):
     lines = []
     for i in range(0, len(lst), items_per_line):
         chunk = lst[i:i + items_per_line]
         line = ", ".join("{!r}".format(x) for x in chunk)
         lines.append(line)
-    return "[" + ",\n ".join(lines) + "]"
+    return print("[" + ",\n ".join(lines) + "]")
+
+
+def userplace(maplist):
+    userrandompos = random.randrange(0, 99, 1)
+
+    maplist[userrandompos] = 'X'
+
+    if userrandompos > 50:
+        randomplacevar = random.randrange(40, 50, 1)
+        randomendpos = random.randrange(0, (userrandompos - randomplacevar), 1)
+    else:
+        randomplacevar = random.randrange(40, 50, 1)
+        randomendpos = random.randrange(49, (userrandompos + randomplacevar), 1)
+
+    maplist[randomendpos] = 'W'
+
+    return userrandompos, randomendpos, maplist
